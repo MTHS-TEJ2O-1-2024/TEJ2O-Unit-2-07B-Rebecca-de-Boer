9fa32a009c1337c265c679ca862ac9cc06b4527b
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-10-01 11:23:37 -0400

    tried to do cookie clicker

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Rebecca de Boer
+ * Created on: Sep 2024
  * This program ...
 */
+basic.clearScreen()
+let cookienumber= 0
+let.cookieIncrease = 1
 
-basic.showString('Hello, World!')
+input.onButtonPressed(
+    (increase.cookienumber + 1))
+    cookieIncrease = cookieIncrease + 1
+)
