8ed89fd2d0f176bea14a49b5f775c20b9228dd0b
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-10-10 11:08:24 -0400

    acts like cookie clicker

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Rebecca de Boer
- * Created on: Sep 2024
- * This program ...
+ * Created on: Oct 2024
+ * This program is cookie clicker
 */
-basic.clearScreen()
-let cookienumber= 0
-let.cookieIncrease = 1
 
-input.onButtonPressed(
-    (increase.cookienumber + 1))
-    cookieIncrease = cookieIncrease + 1
-)
+let cookienumber: number = 0
+
+
+input.onButtonPressed(Button.A, function(){
+    cookienumber = cookienumber + 1
+    basic.showNumber (cookienumber)
+})
